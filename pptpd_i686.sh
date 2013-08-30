4563e440d543cfa9174abaae80356e9a0f873d76
EC2 Default User <ec2-user@ip-172-31-29-227.ap-southeast-1.compute.internal>
2013-08-30 02:55:29 +0000

    script

---
+++ pptpd_i686.sh
@@ -0,0 +1,27 @@
+yum remove -y pptpd ppp
+
+wget https://github.com/wendyeq/pptpd/raw/master/pptpd-1.3.4-2.el6.i686.rpm
+
+yum install -y ppp
+rpm -ivh pptpd-1.3.4-2.el6.i686.rpm
+
+echo "localip 192.168.0.1" >> /etc/pptpd.conf
+echo "remoteip 192.168.0.234-238,192.168.0.245" >> /etc/pptpd.conf
+
+echo "ms-dns 8.8.8.8" >> /etc/ppp/options.pptpd
+echo "ms-dns 8.8.4.4" >> /etc/ppp/options.pptpd
+pass=`openssl rand 6 -base64`
+if [ "$1" != "" ]
+then pass=$1
+fi
+echo "vpn pptpd ${pass} *" >> /etc/ppp/chap-secrets
+
+sed -i 's/net.ipv4.ip_forward=0/net.ipv4.ip_forward=1/' /etc/sysctl.conf
+sed -i 's/net.ipv4.tcp_syncookies = 1/#net.ipv4.tcp_syncookies = 1/' /etc/sysctl.conf
+sysctl -p
+iptables -t nat -A POSTROUTING -s 192.168.0.0/24 -o eth0 -j MASQUERADE
+/etc/init.d/iptables save
+
+chkconfig pptpd on
+service iptables start
+service pptpd start
